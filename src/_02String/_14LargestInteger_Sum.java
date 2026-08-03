9a80ea27b4d288e5664e70f2533247b3f8cff2f9
Anurag Tiwari <anuragtiwari12112004@gmai.com>
2026-08-03 17:50:10 +0530

    Add solution for Largest Integer Sum problem

--- src/_02String/_14LargestInteger_Sum.java
+++ src/_02String/_14LargestInteger_Sum.java
@@ -1,4 +1,59 @@
 package _02String;
 
-public class _14 {
+// LeetCode 4000 - Largest Integer With Given Digit Sum
+
+public class _14LargestInteger_Sum
+{
+    public static int largestInteger(int n, int s)
+    {
+        String Stmax = generate(n + 1);
+        int max = Integer.valueOf(Stmax) - 1;
+
+        for(int i = max; i >= 0; i--)
+        {
+            int sum = digitSum(i);
+
+            if(sum == s)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static String generate(int n)
+    {
+        StringBuilder sb = new StringBuilder("1");
+
+        while(sb.length() < n)
+        {
+            sb.append("0");
+        }
+
+        return sb.toString();
+    }
+
+    public static int digitSum(int n)
+    {
+        int sum = 0;
+
+        while(n != 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args)
+    {
+        int n = 3;
+        int s = 20;
+
+        int ans = largestInteger(n, s);
+
+        System.out.println(ans);
+    }
 }
